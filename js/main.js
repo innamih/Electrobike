2c242f48314ca8bd3376f188a98d2619150ebd89
Inna Rakhmanova <innamih2001@mail.ru>
2022-11-11 19:11:44 +0300

    Download project

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+
+    $('.bike-slider, .slider-items').slick({
+        arrows: false,/*убрали стрелочки*/
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000, /*каждые 2сек картинка меняется*/
+    });
+
+});
